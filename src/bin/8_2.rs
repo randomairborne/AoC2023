3d0bf405a3dc38031b3f0af915effc43d72a350a
valkyrie_pilot <valk@randomairborne.dev>
2023-12-08 00:10:06 -0700

    fix 8-2, big thanks to @tkasriel for explaining why my previous solution was bad and what I could do to fix it, and why that other solution was better

--- src/bin/8_2.rs
+++ src/bin/8_2.rs
@@ -44,27 +44,36 @@ fn main() {
         }
         codes_map.insert(code, CodePair { left, right });
     }
-    let mut states = starting_codes;
-    let mut steps = 0;
-    for direction in pattern.iter().cycle() {
-        steps += 1;
-        let mut all_z = true;
-        for state in states.iter_mut() {
-            let next_pair = codes_map.get(state).unwrap();
+    let mut cycle_times = Vec::new();
+    for starting_code in starting_codes {
+        let mut steps = 0;
+        let mut last = codes_map.get(&starting_code).unwrap();
+        for direction in pattern.iter().cycle() {
+            steps += 1;
             let next = match direction {
-                Direction::Left => &next_pair.left,
-                Direction::Right => &next_pair.right,
+                Direction::Left => &last.left,
+                Direction::Right => &last.right,
             };
-            if next[2] != 'Z' {
-                all_z = false;
+            if next[2] == 'Z' {
+                break;
             }
-            *state = *next;
+            last = codes_map.get(next).unwrap();
         }
-        if all_z {
-            break;
-        }
+        cycle_times.push(steps);
     }
+    eprintln!("{cycle_times:?}");
+    let steps = recursive_lcm(&cycle_times);
     println!("{steps}");
+}
+
+fn recursive_lcm(data: &[i64]) -> i64 {
+    if data.len() < 2 {
+        panic!("recursive_lcm must have at least 2 data points");
+    } else if data.len() == 2 {
+        lcm(data[0], data[1])
+    } else {
+        lcm(data[0], recursive_lcm(&data[1..]))
+    }
 }
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq, Debug)]
@@ -87,4 +96,47 @@ impl Direction {
             _ => panic!("Got {c} neither L nor R"),
         }
     }
+}
+
+// Stolen from num-bigint- reimplementing gcd and lcm was too hard
+
+#[inline]
+fn gcd(a: i64, b: i64) -> i64 {
+    // Use Stein's algorithm
+    let mut m = a;
+    let mut n = b;
+    if m == 0 || n == 0 {
+        return m | n;
+    }
+
+    // find common factors of 2
+    let shift = (m | n).trailing_zeros();
+
+    // divide n and m by 2 until odd
+    m >>= m.trailing_zeros();
+    n >>= n.trailing_zeros();
+
+    while m != n {
+        if m > n {
+            m -= n;
+            m >>= m.trailing_zeros();
+        } else {
+            n -= m;
+            n >>= n.trailing_zeros();
+        }
+    }
+    m << shift
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    gcd_lcm(a, b).1
+}
+
+fn gcd_lcm(a: i64, b: i64) -> (i64, i64) {
+    if a == 0 && b == 0 {
+        return (0, 0);
+    }
+    let gcd = gcd(a, b);
+    let lcm = a * (b / gcd);
+    (gcd, lcm)
 }
