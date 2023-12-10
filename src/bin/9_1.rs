cc5d1181b32f2504ffa7fceb6e4ca05c052fc5f8
valkyrie_pilot <valk@randomairborne.dev>
2023-12-10 00:03:47 -0700

    i give the fuck up

--- src/bin/9_1.rs
+++ src/bin/9_1.rs
@@ -3,39 +3,55 @@ fn main() {
     let mut sum = 0;
     for line in input.lines().map(|v| {
         v.split(' ')
-            .map(|v| v.parse::<usize>().unwrap())
-            .collect::<Vec<usize>>()
+            .map(|v| v.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>()
     }) {
-        sum += process_line(&line);
+        sum += process_line(line);
+        println!();
     }
     println!("{sum}");
 }
 
-fn process_line(line: &[usize]) -> usize {
-    let tree = generate_lines(line);
-    for item in tree {}
+fn process_line(line: Vec<i64>) -> i64 {
+    let mut total = 0;
+    let tree = generate_lines(vec![line]);
+    let mut idx = 1;
+    while idx <= tree.len() - 1 {
+        let line = &tree[idx];
+        println!("{line:?}");
+        let below = &tree[idx - 1];
+        total += extend_line(line, below);
+        idx += 1;
+    }
+    total
 }
 
-fn generate_lines(top: &[usize]) -> Vec<Vec<usize>> {
+fn generate_lines(mut lines: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+    let top = &lines[lines.len() - 1];
     let mut next = Vec::new();
+    let mut all_zeros = true;
     for (idx, item) in top.iter().enumerate() {
         if idx == 0 {
             continue;
         }
+        if *item != 0 {
+            all_zeros = false;
+        }
         next.push(item - top[idx - 1]);
     }
-    if next.iter().all(|v| v == 0) {
-        return;
+    if all_zeros {
+        return lines;
     }
-    generate_lines(&next)
+    lines.push(next);
+    generate_lines(lines)
 }
 
-fn extend_line(existing: &[usize], below: &[usize]) -> usize {
+fn extend_line(existing: &[i64], below: &[i64]) -> i64 {
     if existing.iter().all(|v| *v == 0) {
         return 0;
     }
-    let below_last = existing.len() - 2;
     let existing_last = existing.len() - 1;
+    let below_last = existing.len() - 2;
     let last_existing = existing[existing_last];
     let last_below = below[below_last];
     last_existing + last_below
